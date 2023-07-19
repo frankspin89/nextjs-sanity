c5514fb38235dd3cf743d11961ee81e535f4d159
Frank Spin <info@frankspin.nl>
2023-07-19 15:06:13 +0200

    Update resolvePreviewUrl.ts

--- sanity/structure/resolvePreviewUrl.ts
+++ sanity/structure/resolvePreviewUrl.ts
@@ -4,20 +4,7 @@ import {SanityDocument} from 'sanity'
 import {getSecret, SECRET_ID} from './getSecret'
 
 export default async function resolvePreviewUrl(doc: SanityDocument, client: SanityClient) {
-  let baseUrl = `http://localhost:3001`
-
-  // Use public vars because Studio is all client-side
-  if (process.env.VERCEL) {
-    // This is the URL of the Studio deployment, not the web deployment
-    baseUrl = process.env.NEXT_PUBLIC_SANITY_STUDIO_URL!
-      // process.env.VERCEL_ENV 
-      //   ? `${process.env.NEXT_PUBLIC_SANITY_STUDIO_URL}`
-      //   : // This should work, but doesn't
-      //     // the env seems to be `undefined` in vercel
-      //     // : process.env.SANITY_STUDIO_VERCEL_BRANCH_URL
-      //     // So I'm DIY-ing a branch URL for the web deployment
-      //     ``
-  }
+  let baseUrl = process.env.NEXT_PUBLIC_SANITY_STUDIO_URL
 
   const {_id} = doc
 
